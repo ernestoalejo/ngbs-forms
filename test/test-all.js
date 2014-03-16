e05b9676f016c0c9867360a0be250cb6dc94b4c9
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2014-03-16 14:22:29 +0100

    Implement input validators. Implement select field & validators.

--- test/test-all.js
+++ test/test-all.js
@@ -8,9 +8,20 @@ var form = require('../index'),
 
 
 describe('Full examples: ', function() {
-  it('should generate the min example', function(callback) {
-    var name = 'min-example';
+  // it('should generate the min example', function(callback) {
+  //   var name = 'min-example';
+  //   var generated = form.generate('test/fixtures/' + name + '.form.js');
 
+  //   var expected = fs.readFileSync('test/expected/' + name + '.html');
+  //   mkdirp(path.dirname('tmp/' + name + '.html'), function() {
+  //     fs.writeFileSync('tmp/' + name + '.html', generated);
+  //     expect(generated).to.be(expected.toString());
+  //     callback();
+  //   });
+  // });
+
+  it('should generate the full example', function(callback) {
+    var name = 'full-example';
     var generated = form.generate('test/fixtures/' + name + '.form.js');
 
     var expected = fs.readFileSync('test/expected/' + name + '.html');
@@ -20,19 +31,4 @@ describe('Full examples: ', function() {
       callback();
     });
   });
-
-  // it('should generate the full example', function(callback) {
-  //   var name = 'full-example';
-  //   var contents = fs.readFileSync('test/fixtures/' + name + '.frm');
-  //   var source = form.parse(contents.toString());
-
-  //   var generated = form.generate(source);
-
-  //   var expected = fs.readFileSync('test/expected/' + name + '.html');
-  //   mkdirp(path.dirname('tmp/' + name + '.html'), function() {
-  //     fs.writeFileSync('tmp/' + name + '.html', generated);
-  //     expect(generated).to.be(expected.toString());
-  //     callback();
-  //   });
-  // });
 });
