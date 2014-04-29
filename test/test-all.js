27375d1ac5098627fea89b75674fff4d615b6d52
Rodrigo Valerio <rsvalerio@gmail.com>
2014-04-29 00:05:01 -0300

    Added htmlparser to validate html content instead string content, avoiding breakline break the test

--- test/test-all.js
+++ test/test-all.js
@@ -1,57 +1,70 @@
 'use strict';
 
-var form = require('../index'),
-    fs = require('fs'),
-    mkdirp = require('mkdirp'),
-    path = require('path'),
-    expect = require('expect.js');
+var
+  form       = require('../index'),
+  fs         = require('fs'),
+  mkdirp     = require('mkdirp'),
+  path       = require('path'),
+  htmlparser = require('htmlparser'),
+  expect     = require('expect.js'),
+  handler    = null,
+  parser     = null
+;
 
+function getHtml(html){
+  handler = new htmlparser.DefaultHandler(function () {}, { verbose: false, ignoreWhitespace: true });
+  parser  = new htmlparser.Parser(handler);
+  parser.parseComplete(html);
+  return handler.dom;
+}
+
+function getOptions(name){
+  var options = {};
+  options.generated = form.generate('test/fixtures/' + name + '.form.js');
+  options.htmlGenerated = JSON.stringify( getHtml(options.generated) );
+  options.expected = fs.readFileSync('test/expected/' + name + '.html');
+  options.htmlExpected = JSON.stringify( getHtml(options.expected) );
+
+  return options;
+}
 
 describe('Full examples: ', function() {
   it('should generate the min example', function(callback) {
-    var name = 'min-example';
-    var generated = form.generate('test/fixtures/' + name + '.form.js');
+    var opt = getOptions ('min-example');
 
-    var expected = fs.readFileSync('test/expected/' + name + '.html');
-    mkdirp(path.dirname('tmp/' + name + '.html'), function() {
-      fs.writeFileSync('tmp/' + name + '.html', generated);
-      expect(generated).to.be(expected.toString());
+    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
+      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
+      expect( opt.htmlGenerated ).to.be( opt.htmlExpected );
       callback();
     });
   });
 
   it('should generate the full example', function(callback) {
-    var name = 'full-example';
-    var generated = form.generate('test/fixtures/' + name + '.form.js');
+    var opt = getOptions ('full-example');
 
-    var expected = fs.readFileSync('test/expected/' + name + '.html');
-    mkdirp(path.dirname('tmp/' + name + '.html'), function() {
-      fs.writeFileSync('tmp/' + name + '.html', generated);
-      expect(generated).to.be(expected.toString());
+    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
+      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
+      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
       callback();
     });
   });
 
   it('should generate the name example', function(callback) {
-    var name = 'name-example';
-    var generated = form.generate('test/fixtures/' + name + '.form.js');
+    var opt = getOptions ('name-example');
 
-    var expected = fs.readFileSync('test/expected/' + name + '.html');
-    mkdirp(path.dirname('tmp/' + name + '.html'), function() {
-      fs.writeFileSync('tmp/' + name + '.html', generated);
-      expect(generated).to.be(expected.toString());
+    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
+      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
+      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
       callback();
     });
   });
 
   it('should generate form with no fieldset', function(callback) {
-    var name = 'noFieldset-example';
-    var generated = form.generate('test/fixtures/' + name + '.form.js');
+    var opt = getOptions ('noFieldset-example');
 
-    var expected = fs.readFileSync('test/expected/' + name + '.html');
-    mkdirp(path.dirname('tmp/' + name + '.html'), function() {
-      fs.writeFileSync('tmp/' + name + '.html', generated);
-      expect(generated).to.be(expected.toString());
+    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
+      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
+      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
       callback();
     });
   });
