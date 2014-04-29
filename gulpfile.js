22851c1db4bb7f5ecb276f1ec499d7ec9f5977c4
Rodrigo Valerio <rsvalerio@gmail.com>
2014-04-29 00:11:48 -0300

    Changed jshint.json to .jshintrc to get ide+build integration

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('clean', function() {
 
 gulp.task('lint', function() {
   return gulp.src(['gulpfile.js', 'test/**/*.js', 'lib/**/*.js'])
-    .pipe(jshint('jshint.json'))
+    .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
