3886f3bb13522d7b9917e8041c3c3a41dda5eea4
Rodrigo Valerio <rsvalerio@gmail.com>
2014-05-01 11:30:50 -0300

    Refactored tests and removed htmltags lib(comments for more info)

--- test/test-all.js
+++ test/test-all.js
@@ -5,67 +5,40 @@ var
   fs         = require('fs'),
   mkdirp     = require('mkdirp'),
   path       = require('path'),
-  htmlparser = require('htmlparser'),
-  expect     = require('expect.js'),
-  handler    = null,
-  parser     = null
+  expect     = require('expect.js')
 ;
 
-function getHtml(html){
-  handler = new htmlparser.DefaultHandler(function () {}, { verbose: false, ignoreWhitespace: true });
-  parser  = new htmlparser.Parser(handler);
-  parser.parseComplete(html);
-  return handler.dom;
-}
+function test (name, callback) {
+  
+  var
+    generated = form.generate('test/fixtures/' + name + '.form.js'),
+    expected = fs.readFileSync('test/expected/' + name + '.html')
+  ;
 
-function getOptions(name){
-  var options = {};
-  options.generated = form.generate('test/fixtures/' + name + '.form.js');
-  options.htmlGenerated = JSON.stringify( getHtml(options.generated) );
-  options.expected = fs.readFileSync('test/expected/' + name + '.html');
-  options.htmlExpected = JSON.stringify( getHtml(options.expected) );
+  mkdirp(path.dirname('tmp/' + name + '.html'), function() {
+    fs.writeFileSync('tmp/' + name + '.html', generated);
+    expect( generated ).to.be( expected.toString() );
+    callback();
+  });
 
-  return options;
 }
 
 describe('Full examples: ', function() {
-  it('should generate the min example', function(callback) {
-    var opt = getOptions ('min-example');
 
-    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
-      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
-      expect( opt.htmlGenerated ).to.be( opt.htmlExpected );
-      callback();
-    });
+  it('should generate the min example', function(callback) {
+    test('min-example', callback);
   });
 
   it('should generate the full example', function(callback) {
-    var opt = getOptions ('full-example');
-
-    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
-      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
-      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
-      callback();
-    });
+    test('full-example', callback);
   });
 
   it('should generate the name example', function(callback) {
-    var opt = getOptions ('name-example');
-
-    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
-      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
-      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
-      callback();
-    });
+    test('name-example', callback);
   });
 
   it('should generate form with no fieldset', function(callback) {
-    var opt = getOptions ('noFieldset-example');
-
-    mkdirp(path.dirname('tmp/' + opt.name + '.html'), function() {
-      fs.writeFileSync('tmp/' + opt.name + '.html', opt.generated);
-      expect(opt.htmlGenerated).to.be(opt.htmlExpected);
-      callback();
-    });
+    test('noFieldset-example', callback);
   });
+
 });
